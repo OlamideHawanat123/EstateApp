b4a97a1f1ffbd7324ccd360a9d7599cf37135350
OlamideHawanat123 <raheemhawanat@gmail.com>
2025-05-23 16:08:48 +0100

    Another commit

--- src/dtos/requests/CreateResidentsRequest.java
+++ src/dtos/requests/CreateResidentsRequest.java
@@ -1,4 +1,18 @@
 package dtos.requests;
 
+import data.models.AccessCode;
+import data.models.Resident;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CreateResidentsRequest {
+    private String fullName;
+    private String email;
+    private String phone;
+    private String houseNumber;
+    private String occupation;
+    private Resident resident;
+    private AccessCode accessCode;
 }
